f1442aecdfb533a1463fbcefcc89bccf5b9dc71e
Jefferson Neel <jefferson.neel@scimedsolutions.com>
2018-06-15 15:18:26 -0400

    Make webpacker and yarn work! Add semantic-ui-css and jQuery.

---
+++ config/webpack/environment.js
@@ -0,0 +1,24 @@
+const { environment } = require('@rails/webpacker');
+const webpack = require('webpack');
+
+// resolve-url-loader must be used before sass-loader
+environment.loaders.get('sass').use.splice(-1, 0, {
+  loader: 'resolve-url-loader',
+  options: {
+    attempts: 1
+  }
+});
+
+// Add an ProvidePlugin
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery'
+  })
+);
+
+environment.toWebpackConfig();
+
+module.exports = environment;
